8b4e0f4c81dd38180033840661017d7e5344779c
懒羊羊 <92668369+lanyangyangy@users.noreply.github.com>
2022-03-26 22:05:54 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+void  Nixu(SListNode** pplist)
+{
+    SListNode* p1,*p2,*p3;
+    p1=*pplist;
+    if(p1->next==NULL)
+    {
+        return;
+    }
+    else if(p1->next!=NULL&&p1->next->next!=NULL)
+    {
+        p2=p1->next;
+        p3=p2->next;
+        p2->next=p1;
+        p1=p3;
+        while(1)
+        {
+            if(p1->next!=NULL)
+            {
+                p3=p1->next;
+                p1->next=p2;
+                p2=p1;
+                p1=p3;
+
+            }
+            else
+            {
+                p1->next=p2;
+
+                (*pplist)->next=NULL;
+                (*pplist)=p1;
+                return;
+            }
+        }
+    }
+    else
+    {
+        p2=p1->next;
+        p2->next=p1;
+        p1->next=NULL;
+        *pplist=p2;
+}
+}
+} 
